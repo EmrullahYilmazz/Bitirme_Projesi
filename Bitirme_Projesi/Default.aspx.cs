c94bda3b20f416008fa6ec8972a35eb731e18847
EmrullahYilmazz <korater583@gmail.com>
2021-11-10 14:30:40 +0300

    Proje dosyası ekle.

---
+++ Bitirme_Projesi/Default.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace Bitirme_Projesi
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-GQM48OE;Initial Catalog=Bitirme_Projesi;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            baglanti.Close();
+
+        }
+    }
+}
